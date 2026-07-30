017ce0d2386cd57005c639952cfef59dfb3162db
bulbulyatorrrr099 <bulbulyatorrrr099@gmail.com>
2026-07-30 20:14:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/zabyvashka/',
+  base: '/zabyvashka/', // Обязательно указываем имя репозитория со слэшами
   plugins: [react()],
 })
